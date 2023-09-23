be800ff4d1c6bf45cf3aee6d230a9184769b1da3
Szer13 <shad350@gmail.com>
2023-09-23 14:00:12 +0200

    admin crud

--- src/main/java/com/example/carrentalproject/controller/DepartmentController.java
+++ src/main/java/com/example/carrentalproject/controller/DepartmentController.java
@@ -1,69 +1,72 @@
-//package com.example.carrentalproject.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import project.model.Department;
-//import project.repository.DepartmentRepository;
-//
-//import javax.validation.Valid;
-//import java.util.Optional;
-//
-//@Controller
-//@RequestMapping("/dep/")
-//public class DepartmentController {
-//
-//    private final DepartmentRepository departmentRepository;
-//
-//    public DepartmentController(DepartmentRepository departmentRepository) {
-//        this.departmentRepository = departmentRepository;
-//    }
-//
-//    @GetMapping("/add")
-//    public String displayAddForm(Model model){
-//        model.addAttribute("department", new Department());
-//        return "department/add-form";
-//    }
-//
-//    @PostMapping("/add")
-//    public String processAddForm(@Valid Department department, BindingResult bindingResult){
-//        if(bindingResult.hasErrors()){
-//            return "redirect:/dep/add";
-//        }
-//        departmentRepository.save(department);
-//        return "redirect:/dep/showAll";
-//    }
-//
-//    @RequestMapping("/showAll")
-//    public String showAllDepartments(Model model){
-//        model.addAttribute("departments", departmentRepository.findAll());
-//        return "department/show-all-departments";
-//    }
-//
-//    @GetMapping("/update")
-//    public String displayUpdateForm(@RequestParam Long id, Model model){
-//        Optional<Department> departmentOptional = departmentRepository.findById(id);
-//        departmentOptional.ifPresent(d -> model.addAttribute("department", d));
-//        return "/department/update-form";
-//    }
-//
-//    @PostMapping("/update")
-//    public String processUpdateForm(@Valid Department department, BindingResult bindingResult){
-//        if (bindingResult.hasErrors()){
-//            return "redirect:/dep/update";
-//        }
-//        departmentRepository.save(department);
-//        return "redirect:/dep/showAll";
-//    }
-//
-//    @RequestMapping("/delete")
-//    public String deleteDepartment(@RequestParam Long id){
-//        Optional<Department> optionalDepartment = departmentRepository.findById(id);
-//        optionalDepartment.ifPresent(departmentRepository::delete);
-//        return "redirect:/dep/showAll";
-//    }
-//}
+package com.example.carrentalproject.controller;
+
+import com.example.carrentalproject.model.Department;
+import com.example.carrentalproject.repository.DepartmentRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/admin")
+public class DepartmentController {
+
+    private final DepartmentRepository departmentRepository;
+
+    public DepartmentController(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    @GetMapping("/department/add")
+    public String displayAddForm(Model model){
+        model.addAttribute("department", new Department());
+        return "department/department-add-form";
+    }
+
+    @PostMapping("/department/add")
+    public String processAddForm(@Valid Department department, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "department/department-add-form";
+        }
+        departmentRepository.save(department);
+        return "redirect:/admin/departments";
+    }
+
+    @RequestMapping("/departments")
+    public String showAllDepartments(Model model, @RequestParam(defaultValue = "0") int page){
+        PageRequest pageable = PageRequest.of(page, 10);
+        model.addAttribute("departments", departmentRepository.findAll(pageable));
+        return "department/department-list";
+    }
+
+    @GetMapping("/department/edit")
+    public String displayUpdateForm(@RequestParam Long id, Model model){
+        Optional<Department> departmentOptional = departmentRepository.findById(id);
+        departmentOptional.ifPresent(d -> model.addAttribute("department", d));
+        return "/department/department-edit-form";
+    }
+
+    @PostMapping("/department/edit")
+    public String processUpdateForm(@Valid Department department, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "department/department-edit-form";
+        }
+        departmentRepository.save(department);
+        return "redirect:/admin/departments";
+    }
+
+    @RequestMapping("/department/delete")
+    public String deleteDepartment(@RequestParam Long id){
+        Optional<Department> optionalDepartment = departmentRepository.findById(id);
+        optionalDepartment.ifPresent(departmentRepository::delete);
+        return "redirect:/admin/departments";
+    }
+}
